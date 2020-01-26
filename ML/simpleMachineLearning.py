fd511155f9c79e1ff1aca6abf1eccdc990495ff1
Sam Crawford <sam.crawford82@gmail.com>
2020-01-26 07:41:39 -0500

    Added flask to sml

--- ML/simpleMachineLearning.py
+++ ML/simpleMachineLearning.py
@@ -1,52 +1,49 @@
 import datetime
+from flask import Flask, redirect
+from flask_cors import CORS
 from sklearn.externals import joblib
+from smlHelpers import *
 
-with open('dataset.txt', 'r') as f: 
-    l = f.readlines()
+app = Flask(__name__)
+cors = CORS(app)
 
-dataset = []
+@app.route('/')
+def home():
+    return ("hi", 200)
 
-for line in l:
-    line = line.split()
-    dataset.append({
-            "day" : int(line[0]),
-            "dayOfWeek" : int(line[1]),
-            "hour" : int(line[2]),
-            "available" : int(line[3])
-        })
+@app.route('/api')
+def function():
+    with open('dataset.txt', 'r') as f: 
+        l = f.readlines()
 
-d = datetime.datetime.today()
+    dataset = []
 
-inputDate = int(d.strftime("%d"))
-dow = int(d.strftime("%w"))
-inputTime = int(d.strftime("%H"))
+    for line in l:
+        line = line.split()
+        dataset.append({
+                "day" : int(line[0]),
+                "dayOfWeek" : int(line[1]),
+                "hour" : int(line[2]),
+                "available" : float(line[3])
+            })
 
-def thodeOpen(d, i):
-    if d == 0:
-        return(12 <= i < 22)
-    elif d == 5:
-        return(8 <= i < 17)
-    elif d == 6:
-        return(10 <= i < 17)
+    d = datetime.datetime.today()
+
+    inputDate = int(d.strftime("%d"))
+    dow = int(d.strftime("%w"))
+    inputTime = int(d.strftime("%H"))
+
+    if not thodeOpen(dow, inputTime):
+        return "Thode is currently closed; it opens today at {}.".format(getOpenTime(dow))
     else:
-        return(8 <= i < 22)
+        predictor = joblib.load('finalized_model.sav')
+        s = "This hour, Thode is expected to be {0:5.2f} available.".format(float(predictor.predict(X=[[inputDate, dow, inputTime]])[0]))
+        if thodeOpen(dow, inputTime + 1):
+            s += "Next hour, Thode is expected to be {0:5.2f} available.".format(float(predictor.predict(X=[[inputDate, dow, inputTime + 1]])[0]))
+        return s
 
-def getOpenTime(dow):
-    if dow == 0:
-        return "12:00"       
-    elif dow == 6:
-        return "10:30"
-    else:
-        return "8:00"
-
-if not thodeOpen(dow, inputTime):
-    return "Thode is currently closed; it opens today at {}.".format(getOpenTime(dow))
-else:
-    predictor = joblib.load('finalized_model.sav')
-    s = "This hour, Thode is expected to be {0:5.2f} available.".format(float(predictor.predict(X=[[inputDate, dow, inputTime]])[0]))
-    if thodeOpen(dow, inputTime + 1):
-        s += "Next hour, Thode is expected to be {0:5.2f} available.".format(float(predictor.predict(X=[[inputDate, dow, inputTime + 1]])[0]))
-    return s
+if __name__ == "__main__":
+    app.run(host="localhost", port="8888")
 
 # for i in dataset:
 #     if i["dayOfWeek"] == dow and abs(i["hour"] - inputTime) < 2:
