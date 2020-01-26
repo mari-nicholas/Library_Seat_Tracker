f72eae458f33b2da7c99bd0b0b9c9d88f9580023
unknown <marin@mcmaster.ca>
2020-01-25 20:35:47 -0500

    Fixed Node.js implementation for displaying website

---
+++ Web/myApp.js
@@ -0,0 +1,20 @@
+var express= require("express"), app = express(), http=require("http").Server(app).listen(80);
+
+app.use("/css", express.static("./css"))
+app.use("/img",express.static("./img"))
+console.log("Server started at port 80")
+app.get("/", function(req,res){
+	res.sendFile(__dirname+"/home.html");
+})
+app.get("/home", function(req,res){
+	res.sendFile(__dirname+"/home.html");
+})
+app.get("/page1", function(req,res){
+	res.sendFile(__dirname+"/page1.html");
+})
+app.get("/page2", function(req,res){
+	res.sendFile(__dirname+"/page2.html");
+})
+app.get("/page3", function(req,res){
+	res.sendFile(__dirname+"/page3.html");
+})
