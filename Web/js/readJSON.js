004e1fed2258dcfb9a588be80c843b53d8d1a2ef
unknown <marin@mcmaster.ca>
2020-01-26 05:18:15 -0500

    Connected backend and frontend

---
+++ Web/js/readJSON.js
@@ -0,0 +1,27 @@
+function readJson() {
+	(async () => {
+		var path = '../data/data.json'
+		var xhr = new XMLHttpRequest();
+	    xhr.open('GET', path, true);
+	    xhr.responseType = 'blob';
+	    xhr.onload = function(e) { 
+	      if (this.status == 200) {
+	          var file = new File([this.response], 'temp');
+	          var fileReader = new FileReader();
+	          fileReader.addEventListener('load', function(){
+	          	console.log(fileReader.result);
+				var obj = JSON.parse(fileReader.result);
+				if(obj[0]["lastState"] === 1){
+					document.getElementsByClassName("occupied")[0].style.fill="green";	
+				} else {
+					document.getElementsByClassName("occupied")[0].style.fill="red";
+				}	
+		        });
+	          fileReader.readAsText(file);
+	      } 
+	    }
+	    xhr.send();
+	})();
+}
+
+setInterval(readJson, 30000);
--- Web/myApp.js
+++ Web/myApp.js
@@ -3,6 +3,7 @@ var express= require("express"), app = express(), http=require("http").Server(ap
 app.use("/css", express.static("./css"))
 app.use("/img",express.static("./img"))
 app.use("/js", express.static("./js"))
+app.use("/data", express.static("./data"))
 app.get("/", function(req,res){
 	res.sendFile(__dirname+"/home.html");
 })
@@ -18,3 +19,37 @@ app.get("/page2", function(req,res){
 app.get("/page3", function(req,res){
 	res.sendFile(__dirname+"/page3.html");
 })
+
+function queryData() {
+	//console.log("Test1");
+
+	const MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb+srv://user:user@library-seat-tracker-fvhnk.azure.mongodb.net/test?retryWrites=true&w=majority";
+	const fs = require('fs');
+
+
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var mydb = db.db("seatsAvailable");
+	  var query = { 
+				  $group:
+					{
+						_id: "$seatNo",
+						lastState: { $last: "$available" }
+					}
+				};
+	  mydb.collection("seats").aggregate([query]).toArray(function(err, result) {
+	    if (err) throw err;
+	    //console.log(result);
+	    //console.log(JSON.stringify(result));
+	    fs.writeFileSync('./data/data.json', JSON.stringify(result));
+
+	    db.close();
+	  });
+	});
+
+
+}
+
+
+setInterval(queryData, 30000);
