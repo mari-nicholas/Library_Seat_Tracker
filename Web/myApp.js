beaebed4e6131761492734572c0c8462d446b6be
unknown <marin@mcmaster.ca>
2020-01-26 00:15:46 -0500

    trying to fix node

--- Web/myApp.js
+++ Web/myApp.js
@@ -2,7 +2,7 @@ var express= require("express"), app = express(), http=require("http").Server(ap
 
 app.use("/css", express.static("./css"))
 app.use("/img",express.static("./img"))
-console.log("Server started at port 80")
+app.use("/js", express.static("./js"))
 app.get("/", function(req,res){
 	res.sendFile(__dirname+"/home.html");
 })
